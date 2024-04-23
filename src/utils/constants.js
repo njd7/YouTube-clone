a624418341d01ef6f5df001c7feec51c64748220
njd7 <89265402+njd7@users.noreply.github.com>
2024-04-23 23:47:01 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ export const MENU = "https://img.icons8.com/ios-glyphs/30/menu--v3.png";
 export const SEARCH = "https://img.icons8.com/?size=32&id=XU3XKgdpT0qG&format=png";
 export const USER_ICON = "https://cdn.iconscout.com/icon/free/png-512/free-user-1912184-1617653.png?f=webp&w=256";
 export const NOT_ICON = "https://img.icons8.com/?size=24&id=83193&format=png";
-export const API_KEY = "AIzaSyDNRmpS87c8QDRN7iKwBbzsiQFQ7JYtFp8";
+export const API_KEY = "Sample Key";
 export const YOUTUBE_API_ENDPOINT =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   API_KEY;
